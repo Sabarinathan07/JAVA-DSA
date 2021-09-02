d5d62598b1bed51d1e423e391bfa451e18bfd45d
Sabarinathan07 <64265337+Sabarinathan07@users.noreply.github.com>
2021-09-02 17:11:14 +0530

    singly linked list added

---
+++ List/src/com/sabari/linkedList/Main.java
@@ -0,0 +1,27 @@
+package com.sabari.linkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee sabari = new Employee("Sabari","Nathan",123);
+        Employee adi = new Employee("adi","kumar",123);
+        Employee ash = new Employee("ash","kumar",123);
+        Employee ganesh = new Employee("ganesh","kumar",123);
+
+        EmployeeLinkedList list = new EmployeeLinkedList();
+        list.addToFront(sabari);
+        list.addToFront(adi);
+        list.addToFront(ash);
+        list.addToFront(ganesh);
+
+        list.printList();
+
+
+//        list.removeFromFront();
+//        System.out.println(list.getSize());
+//        list.printList();
+
+
+    }
+}
