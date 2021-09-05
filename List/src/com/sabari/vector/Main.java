3ddbc1e3a0d7a3a8e6d3f94f7c6781261ea96d49
Sabarinathan07 <64265337+Sabarinathan07@users.noreply.github.com>
2021-09-05 16:16:00 +0530

    vector added

---
+++ List/src/com/sabari/vector/Main.java
@@ -0,0 +1,26 @@
+package com.sabari.vector;
+
+import java.util.List;
+import java.util.Vector;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Employee> employeeList = new Vector<>();
+        employeeList.add(new Employee("Jane", "Jones", 123));
+        employeeList.add(new Employee("John", "Doe", 4567));
+        employeeList.add(new Employee("Mary", "Smith", 22));
+        employeeList.add(new Employee("Mike", "Wilson", 3245));
+
+        employeeList.forEach(employee -> System.out.println(employee));
+
+        //By using for loop
+        for(int i=0;i< employeeList.size();i++){
+            System.out.println(employeeList.get(i));
+        }
+
+
+
+    }
+}
