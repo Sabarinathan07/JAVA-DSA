0f5a0ddea030fff91959c6cd91f803b747e6bf79
Sabarinathan07 <64265337+Sabarinathan07@users.noreply.github.com>
2021-09-11 16:08:53 +0530

    Hashtable added

---
+++ Hashtable/src/com/sabari/simpleHashtable/StoredEmployee.java
@@ -0,0 +1,12 @@
+package com.sabari.simpleHashtable;
+
+public class StoredEmployee {
+
+    public String key;
+    public Employee employee;
+
+    public StoredEmployee(String key, Employee employee) {
+        this.key = key;
+        this.employee = employee;
+    }
+}
