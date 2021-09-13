950410f13e86111cb88a71e1be54e14505c2a301
Sabari <r.sabarinathan22@gmail.com>
2021-09-13 14:34:28 +0530

    Heaps added

---
+++ Heaps/src/com/sabari/heapSort/Main.java
@@ -0,0 +1,29 @@
+package com.sabari.heapSort;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Heap heap = new Heap(10);
+
+        heap.insert(80);
+        heap.insert(75);
+        heap.insert(60);
+        heap.insert(68);
+        heap.insert(55);
+        heap.insert(40);
+        heap.insert(52);
+        heap.insert(67);
+
+        heap.printHeap();
+
+        //System.out.println(heap.peek());
+
+        heap.delete(0);
+        heap.printHeap();
+
+        //System.out.println(heap.peek());
+
+        heap.sort();
+        heap.printHeap();
+    }
+}
